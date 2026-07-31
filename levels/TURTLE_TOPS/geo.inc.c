dcfce09369f8beb77424fb1467211f92a86bc4ed
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2026-07-31 15:07:54 -0700

    add the beebo wings into the sky subarea

--- levels/TURTLE_TOPS/geo.inc.c
+++ levels/TURTLE_TOPS/geo.inc.c
@@ -2,3 +2,4 @@
 #include "levels/TURTLE_TOPS/area_2/geo.inc.c"
 #include "levels/TURTLE_TOPS/area_3/geo.inc.c"
 #include "levels/TURTLE_TOPS/area_4/geo.inc.c"
+#include "levels/TURTLE_TOPS/area_5/geo.inc.c"
--- levels/TURTLE_TOPS/leveldata.inc.c
+++ levels/TURTLE_TOPS/leveldata.inc.c
@@ -10,4 +10,7 @@
 #include "levels/TURTLE_TOPS/area_4/collision.inc.c"
 #include "levels/TURTLE_TOPS/area_4/macro.inc.c"
 #include "levels/TURTLE_TOPS/area_4/spline.inc.c"
+#include "levels/TURTLE_TOPS/area_5/collision.inc.c"
+#include "levels/TURTLE_TOPS/area_5/macro.inc.c"
+#include "levels/TURTLE_TOPS/area_5/spline.inc.c"
 #include "levels/TURTLE_TOPS/model.inc.c"

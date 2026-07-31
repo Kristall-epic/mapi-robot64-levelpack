d64e1efd53342b99a92ea5fe380cded3816574cf
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2026-07-31 13:03:38 -0700

    add purple tower subarea

--- levels/TURTLE_TOPS/geo.inc.c
+++ levels/TURTLE_TOPS/geo.inc.c
@@ -1,3 +1,4 @@
 #include "levels/TURTLE_TOPS/area_1/geo.inc.c"
 #include "levels/TURTLE_TOPS/area_2/geo.inc.c"
 #include "levels/TURTLE_TOPS/area_3/geo.inc.c"
+#include "levels/TURTLE_TOPS/area_4/geo.inc.c"
--- levels/TURTLE_TOPS/leveldata.inc.c
+++ levels/TURTLE_TOPS/leveldata.inc.c
@@ -7,4 +7,7 @@
 #include "levels/TURTLE_TOPS/area_3/collision.inc.c"
 #include "levels/TURTLE_TOPS/area_3/macro.inc.c"
 #include "levels/TURTLE_TOPS/area_3/spline.inc.c"
+#include "levels/TURTLE_TOPS/area_4/collision.inc.c"
+#include "levels/TURTLE_TOPS/area_4/macro.inc.c"
+#include "levels/TURTLE_TOPS/area_4/spline.inc.c"
 #include "levels/TURTLE_TOPS/model.inc.c"

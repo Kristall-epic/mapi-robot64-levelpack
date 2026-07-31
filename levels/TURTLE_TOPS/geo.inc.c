359cd252bac6e854a7d4b5136061c1f77c752f7c
Kristall <100389009+Kristall-epic@users.noreply.github.com>
2026-07-31 08:59:43 -0700

    add always wash your hands obby

--- levels/TURTLE_TOPS/geo.inc.c
+++ levels/TURTLE_TOPS/geo.inc.c
@@ -1,2 +1,3 @@
 #include "levels/TURTLE_TOPS/area_1/geo.inc.c"
 #include "levels/TURTLE_TOPS/area_2/geo.inc.c"
+#include "levels/TURTLE_TOPS/area_3/geo.inc.c"
--- levels/TURTLE_TOPS/leveldata.inc.c
+++ levels/TURTLE_TOPS/leveldata.inc.c
@@ -4,4 +4,7 @@
 #include "levels/TURTLE_TOPS/area_2/collision.inc.c"
 #include "levels/TURTLE_TOPS/area_2/macro.inc.c"
 #include "levels/TURTLE_TOPS/area_2/spline.inc.c"
+#include "levels/TURTLE_TOPS/area_3/collision.inc.c"
+#include "levels/TURTLE_TOPS/area_3/macro.inc.c"
+#include "levels/TURTLE_TOPS/area_3/spline.inc.c"
 #include "levels/TURTLE_TOPS/model.inc.c"
